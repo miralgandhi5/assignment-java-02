957f7e74f914c407504e6ae026bd6d1cefdeb515
miralgandhi5 <miralgandhi5@gmail.com>
2018-03-15 11:28:23 +0530

    config file changed.

--- src/main/java/com/knoldus/assignment2/twitterapplication/utilities/TwitterConfig.java
+++ src/main/java/com/knoldus/assignment2/twitterapplication/utilities/TwitterConfig.java
@@ -1,9 +1,9 @@
 package com.knoldus.assignment2.twitterapplication.utilities;
 
 public class TwitterConfig {
-    public static final String consumerKey = "NahdMN1CBic2XHi7rdTMBvWoM";
-    public static final String consumerSecret = "I8SEw6Ez6Fe1slUCbJ1v5JudDdwkcollHuxiZ7C0Bxrggq6itb";
-    public static final String accessSecret = "gzPgNZ5Ub5r8uWgxfohgCU6UN71ZzEV6smT2zGKQ9iDIB";
-    public static final String accessToken = "2983042963-aJN6eM7WmBZ4AbCkuIxPeHKeiPBKJpIiDzfV1jN";
+    public static final String consumerKey = "";
+    public static final String consumerSecret = "";
+    public static final String accessSecret = "";
+    public static final String accessToken = "";
 
 }
